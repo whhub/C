870a0fdab4c0afc8cd082ba68229499af91fa265
whhub <wanghui122004@me.com>
2017-09-19 09:23:45 +0800

    [code snippet] [003] [CallBackHost] Export Class Host

--- CodeSnippet/CallBackHost/Host.h
+++ CodeSnippet/CallBackHost/Host.h
@@ -1,5 +1,12 @@
 #pragma once
-class Host
+
+#ifdef BUILD_CALLBACKHOST_LIB
+#define CallBackHost_Export __declspec (dllexport)
+#else //!defined(MCSF_BUILD_FILMING_DB_LIB)
+#define CallBackHost_Export __declspec (dllimport)
+#endif
+
+class CallBackHost_Export Host
 {
 public:
 	Host(void);
