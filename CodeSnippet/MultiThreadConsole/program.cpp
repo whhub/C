6c7fcf792da65c5d18dcf4858cae2083f82108de
whhub <wanghui122004@me.com>
2017-10-26 13:39:19 +0800

    [code snippet][008][MultiThread] MultiThreadConsole

--- CodeSnippet/MultiThreadConsole/program.cpp
+++ CodeSnippet/MultiThreadConsole/program.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
+#include <boost/thread/thread.hpp>
+
 using namespace std;
+
+void thread_work()
+{
+	cout << "Hello thread" << endl;
+}
+
 
 int main()
 {
+	boost::thread thrd(thread_work);
+	thrd.join();
+
 	cout << "Press Any key to exit" << endl;
 	cin.get();
 	return 0;
