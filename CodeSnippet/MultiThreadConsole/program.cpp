ea1c3a04a699293232137f90c6b74ee1c9dc92f9
whhub <wanghui122004@me.com>
2017-10-27 17:54:04 +0800

    [code snippet][010][MultiThread] function object as thread function

--- CodeSnippet/MultiThreadConsole/program.cpp
+++ CodeSnippet/MultiThreadConsole/program.cpp
@@ -9,11 +9,34 @@ void thread_work()
 	cout << "Hello thread" << endl;
 }
 
+boost::mutex io_mutex;
+
+struct Count
+{
+	Count(int id) : id(id) {}
+
+	void operator()()
+	{
+		for (int i=0; i<10; i++)
+		{
+			boost::mutex::scoped_lock lock(io_mutex);
+
+			cout << id << ": " << i << endl;
+		}
+	}
+
+	int id;
+};
 
 int main()
 {
 	boost::thread thrd(thread_work);
+	boost::thread thrd1(Count(1));
+	boost::thread thrd2(Count(2));
+
 	thrd.join();
+	thrd1.join();
+	thrd2.join();
 
 	cout << "Press Any key to exit" << endl;
 	cin.get();
