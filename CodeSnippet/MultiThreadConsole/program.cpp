8aee04e8c5d2e715de292f126bc72035c186a77d
whhub <wanghui122004@me.com>
2017-10-28 10:07:40 +0800

    [code snippet][011][MultiThread] use boost::bind to transmit parameter

--- CodeSnippet/MultiThreadConsole/program.cpp
+++ CodeSnippet/MultiThreadConsole/program.cpp
@@ -28,15 +28,35 @@ struct Count
 	int id;
 };
 
-int main()
+
+void counting(int id)
+{
+	for (int i=0; i<5; ++i)
+	{
+		boost::mutex::scoped_lock lock(io_mutex);
+		cout << id << ":" << i << endl;
+	}
+}
+
+
+void TestMultiThread()
 {
 	boost::thread thrd(thread_work);
 	boost::thread thrd1(Count(1));
 	boost::thread thrd2(Count(2));
+	boost::thread thrd3(boost::bind(&counting, 3));
+	boost::thread thrd4(boost::bind(&counting, 4));
 
 	thrd.join();
 	thrd1.join();
 	thrd2.join();
+	thrd3.join();
+	thrd4.join();
+}
+
+int main()
+{
+	//TestMultiThread();
 
 	cout << "Press Any key to exit" << endl;
 	cin.get();
